90d45d64980f7ca935fbc458b003f3722e2f956b
Jeff Cho <cjh.lux@gmail.com>
2024-07-24 05:28:08 +0900

    폰트 바꿈

--- Project/Client/CEditorMgr_EditorUI.cpp
+++ Project/Client/CEditorMgr_EditorUI.cpp
@@ -27,13 +27,14 @@ void CEditorMgr::InitImGui()
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     wstring wcontentPath = CPathMgr::GetInst()->GetContentPath();
-    string contentPath = string(wcontentPath.begin(), wcontentPath.end()) + "font\\fontawesome-webfont.ttf";
-    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\malgun.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesKorean());
+    string iconContentPath = string(wcontentPath.begin(), wcontentPath.end()) + "font\\fontawesome-webfont.ttf";
+    string defaultContentPath = string(wcontentPath.begin(), wcontentPath.end()) + "font\\Juri.ttf";
+    io.Fonts->AddFontFromFileTTF(defaultContentPath.c_str(), 20.0f, NULL, io.Fonts->GetGlyphRangesKorean());
     
     ImFontConfig config;
     config.MergeMode = true;
     static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
-    m_IconFont = io.Fonts->AddFontFromFileTTF(contentPath.c_str(), 18.0f, &config, icon_ranges);
+    m_IconFont = io.Fonts->AddFontFromFileTTF(iconContentPath.c_str(), 18.0f, &config, icon_ranges);
     
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
