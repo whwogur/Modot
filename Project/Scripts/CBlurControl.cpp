b4b0b6884a813836547657b7117e10145b996c5a
Jeff Cho <cjh.lux@gmail.com>
2024-09-27 10:37:37 +0900

    FIX GodRay 방식 변경

--- Project/Scripts/CBlurControl.cpp
+++ Project/Scripts/CBlurControl.cpp
@@ -5,10 +5,9 @@ CBlurControl::CBlurControl()
 	: CScript(SCRIPT_TYPE::BLURCONTROL)
 {
 	AddScriptParam(SCRIPT_PARAM::BOOLEAN_TOGGLE, u8"효과 활성화", &m_Activated);
-	AddScriptParam(SCRIPT_PARAM::FLOAT, u8"감소", &m_Decay);
+	AddScriptParam(SCRIPT_PARAM::FLOAT, u8"타이머", &m_Timer);
 	AddScriptParam(SCRIPT_PARAM::FLOAT, u8"밀도", &m_Density);
 	AddScriptParam(SCRIPT_PARAM::FLOAT, u8"감소 가중치", &m_Weight);
-	AddScriptParam(SCRIPT_PARAM::FLOAT, u8"고유 감소", &m_TargetDecay);
 	AddScriptParam(SCRIPT_PARAM::INT, u8"종류", &m_Effect, 0, 0, u8"0: 쎔, 1: 약함");
 }
 
@@ -21,24 +20,12 @@ void CBlurControl::Tick()
 {
 	if (m_Activated)
 	{
-		if (m_Acc < m_Timer)
+		if (0.f < m_Timer)
 		{
-			m_Acc += DT;
+			m_Timer -= DT;
 			MeshRender()->GetDynamicMaterial()->SetScalarParam(INT_3, 1);
 
-			if (m_Decay > m_TargetDecay)
-				m_Peaked = true;
-
-			if (m_Peaked)
-			{
-				m_Decay -= DT;
-			}
-			else
-			{
-				m_Decay += DT;
-			}
-
-			MeshRender()->GetDynamicMaterial()->SetScalarParam(FLOAT_0, m_Decay);
+			MeshRender()->GetDynamicMaterial()->SetScalarParam(FLOAT_0, m_Timer);
 			MeshRender()->GetDynamicMaterial()->SetScalarParam(FLOAT_1, m_Density);
 			MeshRender()->GetDynamicMaterial()->SetScalarParam(FLOAT_2, m_Weight);
 		}
@@ -66,26 +53,21 @@ void CBlurControl::LoadFromFile(FILE* _File)
 void CBlurControl::Activate()
 {
 	m_Activated = true;
-	m_Peaked = false;
-	m_Acc = 0.f;
 	
 	switch (m_Effect)
 	{
-	case BlurEffect::Lineth:
+	case BlurEffect::Bright:
 	{
-		m_Timer = 3.f;
-		m_TargetDecay = 1.5f;
+		m_Timer = 2.f;
 		break;
 	}
-	case BlurEffect::Demon:
+	case BlurEffect::LessBright:
 	{
 		m_Timer = 1.5f;
-		m_TargetDecay = 0.8f;
 		break;
 	}
 	}
 
-	m_Decay = 0.f;
 	m_Density = 0.5f;
 	m_Weight = 0.1f;
 }
--- Project/Scripts/CBlurControl.h
+++ Project/Scripts/CBlurControl.h
@@ -3,7 +3,7 @@
 
 enum class BlurEffect
 {
-    Lineth = 0, Demon = 1,
+    Bright = 0, LessBright = 1,
 };
 
 class CBlurControl :
@@ -22,15 +22,12 @@ public:
 public:
     void Activate();
 private:
-    BlurEffect  m_Effect = BlurEffect::Lineth;
+    BlurEffect  m_Effect = BlurEffect::Bright;
     float       m_Decay = 0.97f;
     float       m_Density = 0.5f;
     float       m_Weight = 0.1f;
     bool        m_Activated = false;
-    bool        m_Peaked = false;
-
-    float       m_TargetDecay = 0.f;
-    float m_Acc = 0.f;
-    float m_Timer = 0.f;
+    
+    float       m_Timer = 0.f;
 };
 
