a1bd837a4149d157b63148a4a10739a123d7f9f6
whwogur <cjh.lux@gmail.com>
2024-10-17 14:22:59 +0900

    ADD DeferredMRT LightMRT

--- Project/Engine/CRenderMgr_Init.cpp
+++ Project/Engine/CRenderMgr_Init.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CRenderMgr.h"
 #include "CAssetMgr.h"
+#include "CDevice.h"
 #include "components.h"
 #include "CMRT.h"
 
@@ -61,5 +62,48 @@ void CRenderMgr::CreateMRT()
 		m_arrMRT[(UINT)MRT_TYPE::EFFECT_BLUR]->SetName(L"EffectBlur");
 		m_arrMRT[(UINT)MRT_TYPE::EFFECT_BLUR]->Create(1, arrRT, nullptr);
 		m_arrMRT[(UINT)MRT_TYPE::EFFECT_BLUR]->SetClearColor(arrClearColor, false);
+	}
+
+
+	// ===============
+	// Deferred MRT
+	// ===============
+	Vec2 vResolution = CDevice::GetInst()->GetResolution();
+	{
+		Ptr<CTexture> arrRT[8] =
+		{
+
+			CAssetMgr::GetInst()->CreateTexture(L"AlbedoTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R8G8B8A8_UNORM, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+			CAssetMgr::GetInst()->CreateTexture(L"NormalTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+			CAssetMgr::GetInst()->CreateTexture(L"PositionTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+			CAssetMgr::GetInst()->CreateTexture(L"EmissiveTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+			CAssetMgr::GetInst()->CreateTexture(L"DataTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE)
+		};
+		Ptr<CTexture> pDSTex = CAssetMgr::GetInst()->FindAsset<CTexture>(L"DepthStencilTex");
+		Vec4		  arrClearColor[8] = { Vec4(0.f, 0.f, 0.f, 0.f), };
+
+		m_arrMRT[(UINT)MRT_TYPE::DEFERRED] = std::make_unique<CMRT>();
+		m_arrMRT[(UINT)MRT_TYPE::DEFERRED]->SetName(L"Deferred");
+		m_arrMRT[(UINT)MRT_TYPE::DEFERRED]->Create(5, arrRT, pDSTex);
+		m_arrMRT[(UINT)MRT_TYPE::DEFERRED]->SetClearColor(arrClearColor, false);
+	}
+
+	// ==========
+	// Light MRT
+	// ==========
+	{
+		Ptr<CTexture> arrRT[8] =
+		{
+
+			CAssetMgr::GetInst()->CreateTexture(L"DiffuseTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+			CAssetMgr::GetInst()->CreateTexture(L"SpecularTargetTex", (UINT)vResolution.x, (UINT)vResolution.y, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE),
+		};
+		Ptr<CTexture> pDSTex = CAssetMgr::GetInst()->FindAsset<CTexture>(L"DepthStencilTex");
+		Vec4		  arrClearColor[8] = { Vec4(0.f, 0.f, 0.f, 0.f), };
+
+		m_arrMRT[(UINT)MRT_TYPE::LIGHT] = std::make_unique<CMRT>();
+		m_arrMRT[(UINT)MRT_TYPE::LIGHT]->SetName(L"Light");
+		m_arrMRT[(UINT)MRT_TYPE::LIGHT]->Create(2, arrRT, pDSTex);
+		m_arrMRT[(UINT)MRT_TYPE::LIGHT]->SetClearColor(arrClearColor, false);
 	}
 }
