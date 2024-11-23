752220476c62a0884ca4b811a06dcecd44de532b
Jeff Cho <cjh.lux@gmail.com>
2024-11-23 17:21:40 +0900

    CHORE 헬퍼클래스 생성자 삭제

--- Project/Client/ModotHelpers.h
+++ Project/Client/ModotHelpers.h
@@ -4,6 +4,10 @@
 class ModotHelpers
 {
 public:
+	ModotHelpers() = delete;
+	ModotHelpers(const ModotHelpers&) = delete;
+	ModotHelpers& operator=(const ModotHelpers&) = delete;
+public:
 	static bool FilledSpinner(const char* _Label, float _Radius, int _Thickness, const ImU32& _Color);
 	static bool BufferingBar(const char* _Label, float _Value, const ImVec2& _Size, const ImU32& _BG, const ImU32& _FG);
 	static void DrawVec3Control(const string& _Label, Vec3& _Values, float _ResetValue = 0.0f, float _ColumnWidth = 100.0f);
